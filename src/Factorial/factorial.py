34ba9d3fb2274617983fca6edce36e1140116b6d
Lucas Calivari <lcalivari@gmail.com>
2026-03-20 16:24:05 -0300

    Se añade la funcion de aceptar rangos desde-hasta.

--- src/Factorial/factorial.py
+++ src/Factorial/factorial.py
@@ -6,6 +6,7 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
+
 def factorial(num): 
     if num < 0: 
         print("Factorial de un número negativo no existe")
@@ -20,11 +21,27 @@ def factorial(num):
             num -= 1
         return fact 
 
- #La condición if len(sys.argv) == 0 nunca se cumplirá porque sys.argv siempre tiene al menos un elemento (el nombre del script). Debes verificar que haya 2 argumentos (el script + el número).
+# La condición if len(sys.argv) == 0 nunca se cumplirá porque sys.argv siempre tiene al menos un elemento (el nombre del script). Debes verificar que haya 2 argumentos (el script + el número).
 
 if len(sys.argv) < 2:
-   print("Debe informar un número!")
+   print("Debe informar un número o rango (ej. 4-8)!")
    sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
 
+# Verificar si el argumento contiene un guión (rango)
+if '-' in sys.argv[1]:
+    # Procesar rango
+    partes = sys.argv[1].split('-')
+    inicio = int(partes[0])
+    fin = int(partes[1])
+    
+    if inicio > fin:
+        print("El número inicial debe ser menor o igual al final")
+        sys.exit()
+    
+    print(f"Calculando factoriales desde {inicio} hasta {fin}:")
+    for num in range(inicio, fin + 1):
+        print("Factorial ", num, "! es ", factorial(num))
+else:
+    # Procesar número único
+    num = int(sys.argv[1])
+    print("Factorial ", num, "! es ", factorial(num))
