414ab1fcef7f2861f701cb072e745de7aaf4a08e
Lucas Calivari <lcalivari@gmail.com>
2026-03-20 16:26:53 -0300

    Ahora se acepta numeros sin rango limite de desde-hasta

--- src/Factorial/factorial.py
+++ src/Factorial/factorial.py
@@ -24,23 +24,42 @@ def factorial(num):
 # La condición if len(sys.argv) == 0 nunca se cumplirá porque sys.argv siempre tiene al menos un elemento (el nombre del script). Debes verificar que haya 2 argumentos (el script + el número).
 
 if len(sys.argv) < 2:
-   print("Debe informar un número o rango (ej. 4-8)!")
+   print("Debe informar un número o rango (ej. 4-8, -10, 5-)!")
    sys.exit()
 
 # Verificar si el argumento contiene un guión (rango)
 if '-' in sys.argv[1]:
     # Procesar rango
     partes = sys.argv[1].split('-')
-    inicio = int(partes[0])
-    fin = int(partes[1])
     
-    if inicio > fin:
-        print("El número inicial debe ser menor o igual al final")
-        sys.exit()
+    # Caso "-hasta" (ej. -10)
+    if partes[0] == '':
+        fin = int(partes[1])
+        inicio = 1
+        print(f"Calculando factoriales desde {inicio} hasta {fin}:")
+        for num in range(inicio, fin + 1):
+            print("Factorial ", num, "! es ", factorial(num))
     
-    print(f"Calculando factoriales desde {inicio} hasta {fin}:")
-    for num in range(inicio, fin + 1):
-        print("Factorial ", num, "! es ", factorial(num))
+    # Caso "desde-" (ej. 5-)
+    elif partes[1] == '':
+        inicio = int(partes[0])
+        fin = 60
+        print(f"Calculando factoriales desde {inicio} hasta {fin}:")
+        for num in range(inicio, fin + 1):
+            print("Factorial ", num, "! es ", factorial(num))
+    
+    # Caso rango normal "desde-hasta"
+    else:
+        inicio = int(partes[0])
+        fin = int(partes[1])
+        
+        if inicio > fin:
+            print("El número inicial debe ser menor o igual al final")
+            sys.exit()
+        
+        print(f"Calculando factoriales desde {inicio} hasta {fin}:")
+        for num in range(inicio, fin + 1):
+            print("Factorial ", num, "! es ", factorial(num))
 else:
     # Procesar número único
     num = int(sys.argv[1])
